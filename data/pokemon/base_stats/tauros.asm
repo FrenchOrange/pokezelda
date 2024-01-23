d301fc16c3944055b9d21b2b95bedc4796a12f71
FrenchOrange <64930003+FrenchOrange@users.noreply.github.com>
2024-01-23 04:59:18 +0100

    Tauros oopsie.

--- data/pokemon/base_stats/tauros.asm
+++ data/pokemon/base_stats/tauros.asm
@@ -4,7 +4,7 @@
 	;   hp  atk  def  spd  sat  sdf
 
 	db NORMAL, NORMAL ; type
-	db 55 ; catch rate
+	db 45 ; catch rate
 	db 211 ; base exp
 	db NO_ITEM, NO_ITEM ; items
 	db GENDER_F0 ; gender ratio
